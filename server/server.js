158df12ca642566e91aeda847610d10f091332be
Your Name <sheel.bedi@gmail.com>
2016-12-02 15:22:16 -0600

    server connection established

--- server/server.js
+++ server/server.js
@@ -47,3 +47,51 @@ app.get('/fetchData',function(req,res){
 })
 app.listen(port,ip);
 console.log("Listening to port :4040");
+
+
+//DB Stuff below 
+var options = {
+  user: googleAPI.user,
+  pass: googleAPI.pass
+};
+
+var mongoose = require('mongoose');
+mongoose.connect(googleAPI.dbUrl, options);
+ 
+var db = mongoose.connection;
+ 
+db.on('error', function (err) {
+console.log('connection error', err);
+});
+db.once('open', function () {
+console.log('connected to server.');
+});
+
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+name : String,
+age : Number,
+DOB : Date,
+isAlive : Boolean
+});
+
+userSchema.methods.isYounger = function () {
+return this.model('User').age < 50 ? true : false;
+};
+
+var User = mongoose.model('User', userSchema);
+
+var arvind = new User({
+name : 'Arvind',
+age : 99,
+DOB : '01/01/1915',
+isAlive : true
+});
+ 
+arvind.save(function (err, data) {
+if (err) console.log(err);
+else console.log('Saved : ', data );
+});
+
+console.log('isYounger : ',arvind.isYounger());
