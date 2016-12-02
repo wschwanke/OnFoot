7f98acf6ed8f13469904d64d736c33b37e1ecfa2
Ethan <ethan4e@gmail.com>
2016-12-01 19:34:38 -0600

    About to merge new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import './css/App.css';
- 
-import List from './List';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Geolocation.js'
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1> Welcome to On-Foot! </h1>
-      <List/>
-    </div>
+
+
+class App extends Component {
+  getLocation() {
+    if (navigator.geolocation) {
+      console.log('Geolocation is supported!');
+      return '';
+      }
+      else {
+      console.log('Geolocation is not supported for this Browser/OS.');
+      }
+    // Get the user's location:
+ if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(function(position) {
+     return "latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+     console.log("Success! latitude: ", position.coords.latitude, "longitude:", position.coords.longitude)
+       .done(function(data) {
+           $('body').append("#dummy-restaurant");
+})
+   });
+ }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+
+          To get started, edit <code>src/App.js</code> {this.getLocation()}and save to reload.
+        </p>
+      </div>
     );
-};
+  }
+}
 
 export default App;
