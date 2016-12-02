91285c6761f85e9ab3126cd8b707687d7aceb77e
Ethan <ethan4e@gmail.com>
2016-12-01 20:29:51 -0600

    kinda renders on page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,16 @@ import './css/App.css';
 
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      location: `${this.getLocation.apply(this)}`
+    };
+  }
+
   getLocation() {
+    var that = this
+    console.log(that)
     if (navigator.geolocation) {
       console.log('Geolocation is supported!');
      // return '';
@@ -14,19 +23,22 @@ class App extends Component {
       console.log('Geolocation is not supported for this Browser/OS.');
       }
     // Get the user's location:
- if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(position) {
-    console.log("Success! latitude: ", position.coords.latitude, "longitude:", position.coords.longitude)
-     return "latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Success! latitude: ", position.coords.latitude, "longitude:", position.coords.longitude)
+        console.log(this)
+        
+        that.setState({location :  `latitude: " + {position.coords.latitude} + "<br>longitude: " + {position.coords.longitude}`})
      
        //.done(function(data) {
           // $('body').append("#dummy-restaurant");
 //})
-   });
- }
+      });
+    }
   }
 
   render() {
+    var location = this.state.location
     return (
       <div className="App">
         <div className="App-header">
@@ -35,7 +47,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
 
-          To get started, edit <code>src/App.js</code> {this.getLocation()}and save to reload.
+          To get started, edit <code>src/App.js</code> {location}and save to reload.
         </p>
       </div>
     );
