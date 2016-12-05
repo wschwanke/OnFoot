0073ad338270b156e38b40bf5ce1eb7b4dfe59e9
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-05 10:40:37 -0600

    small before pushing up correct styling etc

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ import Item from './Item';
 const List = ({data}) => {
   return (
     <div className="list">
-        <div className="list-header-container"><h3>Get there on foot!:</h3></div>
+        <div className="list-header-container"><h3>Get there on foot!</h3></div>
          <main role="main">
           <ul className="flexgrid columns-news">
         
